16ceee8ab94184f2452034dbbaa542159fd1a192
Sage <salsaeed999@gmail.com>
2022-11-20 22:14:01 -0500

    corrected the tab spacing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  year = new Date().getFullYear();
+	year = new Date().getFullYear();
 }
